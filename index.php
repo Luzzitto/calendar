91b8b91309b2f68c41fd75572be61201f6332ef6
Luzzitto <luzzittot@outlook.com>
2022-09-27 14:43:57 -0400

    Project Initialization

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calendar</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fullcalendar/lib/main.min.css">
+</head>
+
+<body>
+
+    <div id="calendar"></div>
+
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/fullcalendar/lib/main.min.css"></script>
+</body>
+
+</html>
